e62a90bc7d9b57abbcc9039ec6ee732144814240
AlfredoHC07 <alfredo09hernandez@gmail.com>
2023-04-20 14:27:13 -0400

    first commit

--- pharmacy/views/login.py
+++ pharmacy/views/login.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from sic_app.models import CustomUser
+from pharmacy.models import CustomUser
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
