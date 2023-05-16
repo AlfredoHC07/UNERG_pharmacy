3dd7de78c3448583e660fbcfcc38d5dd078f7365
AlfredoHC07 <alfredo09hernandez@gmail.com>
2023-05-16 11:50:16 -0400

    [FIX]

--- pharmacy/views/product.py
+++ pharmacy/views/product.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
-from .login import login_required
 from pharmacy.models import Producto
 
 # Create your views here.
-@login_required
 def product(request):
     context = {}
     context['productos'] = Producto.objects.all()
